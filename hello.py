274a91d23a72d1a4a8fe9cf000cfb9dbd3678afe
kxrtx <90280208+kxrtx@users.noreply.github.com>
2023-09-22 12:37:37 -0400

    Update hello.py with the code from task 3

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('index.html', name=name)
+
+@app.route('/')
+def index():
+    print(datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
