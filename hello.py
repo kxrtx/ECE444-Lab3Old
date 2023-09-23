3584b0ba3417116e975e44fd9ca6282a123fbdf9
kxrtx <90280208+kxrtx@users.noreply.github.com>
2023-09-23 17:03:02 -0400

    Update hello.py in terms of activity 4

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, session, redirect, url_for , flash
+from flask import request
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import Email, DataRequired, ValidationError
 
 
 app = Flask(__name__)
@@ -13,8 +14,13 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+def validate_email(form, field):
+    if "@" not in field.data:
+        raise ValidationError(f"Please include '@' in the email address. {field.data} is missing an '@'")
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email adderess?', validators=[validate_email])
     submit = SubmitField('Submit')
 
 
@@ -28,12 +34,21 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         name = form.name.data
+        email = form.email.data
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name') , email=session.get('email'))
+
+@app.errorhandler(404)
+def page_notfound(e):
+    return render_template('404.html'), 404
 
 
 
